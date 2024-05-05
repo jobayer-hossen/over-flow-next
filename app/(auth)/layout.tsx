ce2e2ed4e6c864fe9271857d8524840983bdf15d
emonhasan007 <emon45088@gmail.com>
2024-05-05 17:52:32 +0600

    tag action added

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Layout = ({children}:{children:React.ReactNode}) => {
 
     return (
-        <div>
+        <div className="flex min-h-screen w-full items-center justify-center">
             {children}
         </div>
     );
